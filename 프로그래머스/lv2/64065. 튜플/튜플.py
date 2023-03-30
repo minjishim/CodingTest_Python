ccd9777d881311d1efc821c83c0b817fadc9ad02
JA <37624550+minjishim@users.noreply.github.com>
2023-03-30 23:50:38 +0900

    [level 2] Title: 튜플, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -3,7 +3,6 @@ import re
 from collections import Counter
 
 def solution(s):
-    answer = []
     m = Counter(re.findall('\d+', s))
     values, _ = zip(*m.most_common())
     answer = list(map(int, list(values)))
